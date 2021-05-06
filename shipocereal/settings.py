12eadd8995671627b2a4bed742555262423d04f1
arnort20 <70693271+arnort20@users.noreply.github.com>
2021-05-06 13:14:21 +0000

    .

--- shipocereal/settings.py
+++ shipocereal/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'shipocereal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store',
+        'PASSWORD': 'shipocereal',
+        'USER': 'shipocereal',
+        'HOST': '34.76.235.96',
+        'PORT': '5432'
     }
 }
 
