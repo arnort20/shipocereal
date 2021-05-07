46033cf49e38e4af901ef6626992b347853c62c8
Rut Tryggvadóttir <rutt20@ru.is>
2021-05-07 16:00:51 +0000

    aaa

---
+++ manage_BACKUP_21928.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shipocereal.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+<<<<<<< HEAD
+=======
+        #Eftir síðasta Pull kom error á pyr.raise ImportError( Þegar strokað var
+        # Pyr. út hætti hann. Muna það
+>>>>>>> 0b53bb2293f18d3f0601ceb0235ea5b1a07339e9
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
+
+
