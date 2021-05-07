0b53bb2293f18d3f0601ceb0235ea5b1a07339e9
ArnarDecaY <arnarj19@ru.is>
2021-05-07 14:37:02 +0000

    Breytingar á nav komið

--- ship_o_cereal/views.py
+++ ship_o_cereal/views.py
@@ -5,4 +5,4 @@ def home(request):
     return render(request, 'homepage/home.html')
 
 def prod_view(request):
-    return render(request, 'store/ProductView')
+    return render(request, 'store/product_view.html')
