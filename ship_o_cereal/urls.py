579ef9c6d0fde880e318b743111b22c042b5edb2
Rut Tryggvadóttir <rutt20@ru.is>
2021-05-09 20:09:17 +0000

    tengdi alla síðuna

--- ship_o_cereal/urls.py
+++ ship_o_cereal/urls.py
@@ -4,6 +4,12 @@ from ship_o_cereal import views
 urlpatterns = [
     # http//localhost:8000
     path('', views.home, name="home"),
-    path('store/', views.prod_view, name="ProductView")
-
+    path('store/', views.prod_view, name="ProductView"),
+    path('merch/',views.merch_view,name="MerchView"),
+    path('bowl/',views.bowl_view,name="BowlView"),
+    path('cart/',views.cart_view,name="CartView"),
+    path('checkout/',views.checkout_view,name="CheckoutView"),
+    path('login/',views.login_view,name="LoginView"),
+    path('signup/',views.signup_view,name="SignupView"),
+    path('user/',views.user_view,name="UserView")
 ]
--- ship_o_cereal/views.py
+++ ship_o_cereal/views.py
@@ -6,3 +6,27 @@ def home(request):
 
 def prod_view(request):
     return render(request, 'store/product_view.html')
+
+def merch_view(request):
+    return render(request, 'store/merch.html')
+
+def bowl_view(request):
+    return render(request, 'store/subcategory.html')
+
+def cart_view(request):
+    return render(request, 'store/cart.html')
+
+def checkout_view(request):
+    return render(request, 'store/checkout.html')
+
+def login_view(request):
+    return render(request, 'store/login.html')
+
+def signup_view(request):
+    return render(request, 'store/signup.html')
+
+def user_view(request):
+    return render(request, 'store/user.html')
+
+def frontPage(request):
+    return render(request, '')
