f1cf7d7efc7b0188cb8b104bfa3b3c576205355a
arnort20 <70693271+arnort20@users.noreply.github.com>
2021-05-10 13:02:32 +0000

    getur sótt vöru eftir ID

--- ship_o_cereal/urls.py
+++ ship_o_cereal/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from ship_o_cereal import views
 
 urlpatterns = [
     # http//localhost:8000
     path('', views.home, name="home"),
     path('store/', views.prod_view, name="ProductView"),
+    path('store/<int:productId>', views.prod_by_id, name="ProductPage"),
     path('merch/',views.merch_view,name="MerchView"),
     path('bowl/',views.bowl_view,name="BowlView"),
     path('cart/',views.cart_view,name="CartView"),
--- ship_o_cereal/views.py
+++ ship_o_cereal/views.py
@@ -1,32 +1,49 @@
 from django.shortcuts import render
+from ship_o_cereal.models import *
 
 
 def home(request):
     return render(request, 'homepage/home.html')
 
+
 def prod_view(request):
-    return render(request, 'store/product_view.html')
+    # allProducts = Products.objects.all()
+    honeyStars = {'product': Products.objects.get(pk=1)}
+    return render(request, 'store/product_view.html', context=honeyStars)
+
+
+def prod_by_id(request, productId):
+    context = {'product': Products.objects.get(pk=productId)}
+    return render(request, 'store/product_view.html', context=context)
+
 
 def merch_view(request):
     return render(request, 'store/merch.html')
 
+
 def bowl_view(request):
     return render(request, 'store/subcategory.html')
+
 
 def cart_view(request):
     return render(request, 'store/cart.html')
 
+
 def checkout_view(request):
     return render(request, 'store/checkout.html')
+
 
 def login_view(request):
     return render(request, 'store/login.html')
 
+
 def signup_view(request):
     return render(request, 'store/signup.html')
 
+
 def user_view(request):
     return render(request, 'store/user.html')
+
 
 def frontPage(request):
     return render(request, '')
