204a28bc2a2ed6aeae64fe2f35832a353d70d77a
arnort20 <70693271+arnort20@users.noreply.github.com>
2021-05-10 14:34:27 +0000

    .

--- user/views.py
+++ user/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,3 +6,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'store/login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+
+    return render(request, 'user/register.html', {
+        'form': UserCreationForm()
+    })
