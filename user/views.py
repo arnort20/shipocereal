1b072ddd801b64f81547300356b14ef7691b9501
Rut Tryggvadóttir <rutt20@ru.is>
2021-05-11 13:35:45 +0000

    laga user.views

--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from 
+#from
 from user.forms.profile_form import ProfileForm
 from user.models import Profile
 
@@ -26,8 +26,8 @@ def user_view(request):
 
 
 def login(request):
-    if request.method == 'POST':
-        form =
+    #if request.method == 'POST':
+    #    form =
     return render(request, 'user/login.html')
 
 
