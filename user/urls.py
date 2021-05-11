2a52920df7d8f1c83dc6896fa99375a3f46869f0
arnort20 <70693271+arnort20@users.noreply.github.com>
2021-05-11 11:35:15 +0000

    urls löguð

--- user/urls.py
+++ user/urls.py
@@ -4,6 +4,11 @@ from user import views
 
 urlpatterns = [
     path('', views.login, name="login"),
-    path('register/', views.register, name='register')
+    # path('register/', views.register, name='register')
+    path('cart/',views.cart_view,name="CartView"),
+    path('checkout/',views.checkout_view,name="CheckoutView"),
+    path('login/',views.login_view,name="LoginView"),
+    path('signup/',views.signup_view,name="SignupView"),
+    path('user/',views.user_view,name="UserView")
     # path('profile/')
 ]
--- user/views.py
+++ user/views.py
@@ -4,33 +4,53 @@ from user.forms.profile_form import ProfileForm
 from user.models import Profile
 
 
+def cart_view(request):
+    return render(request, 'store/cart.html')
+
+
+def checkout_view(request):
+    return render(request, 'store/checkout.html')
+
+
+def login_view(request):
+    return render(request, 'store/login.html')
+
+
+def signup_view(request):
+    return render(request, 'store/signup.html')
+
+
+def user_view(request):
+    return render(request, 'store/user.html')
+
+
 def login(request):
     return render(request, 'store/login.html')
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    return render(request, 'user/register.html', {
-        'form': UserCreationForm()
-    })
-
-
-def profile(request):
-    profile = Profile.objects.filter(user=request.user).first()
-    if request.method == 'POST':
-        form = ProfileForm(instance=profile, data=request.POST)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.user = request.user
-            profile.save()
-            return redirect('profile')
-    return render(request, 'user/profile.html', {
-        'form': ProfileForm(instance=profile)
-    })
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('login')
+#     return render(request, 'user/register.html', {
+#         'form': UserCreationForm()
+#     })
+#
+#
+# def profile(request):
+#     profile = Profile.objects.filter(user=request.user).first()
+#     if request.method == 'POST':
+#         form = ProfileForm(instance=profile, data=request.POST)
+#         if form.is_valid():
+#             profile = form.save(commit=False)
+#             profile.user = request.user
+#             profile.save()
+#             return redirect('profile')
+#     return render(request, 'user/profile.html', {
+#         'form': ProfileForm(instance=profile)
+#     })
 
 
 
