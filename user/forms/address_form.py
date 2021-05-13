7799d98c070358add302db6f36227f38208e5efb
Rut Tryggvadóttir <rutt20@ru.is>
2021-05-13 11:50:57 +0000

    haha

--- user/forms/address_form.py
+++ user/forms/address_form.py
@@ -1,16 +1,19 @@
 from django.forms import ModelForm, widgets
 from ship_o_cereal.models import Addresses
 from django_countries.widgets import CountrySelectWidget
+from django_countries.fields import CountryField
 
 
 class AddressCreateForm(ModelForm):
+    country = CountryField(blank_label='Select a country').formfield(widget=CountrySelectWidget(attrs={"class": "form-control address__input"}))
     class Meta:
         model = Addresses
-        exclude = ['addrId', 'userId','country']
+        exclude = ['addrId', 'userId']
         widgets = {
-            'street': widgets.TextInput(attrs={'class': 'form-control address__input'}),
+            'address': widgets.TextInput(attrs={'class': 'form-control address__input'}),
             'apt_num': widgets.NumberInput(attrs={'class': 'form-control address__input-small'}),
-            'zip': widgets.NumberInput(attrs={'class': 'form-control address__input-small'})
+            'zip': widgets.NumberInput(attrs={'class': 'form-control address__input-small'}),
+            #'country' : CountrySelectWidget()
         }
 
 
