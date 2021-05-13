f2204dd7985eec78481bd141590ee8ba2ca676f2
arnort20 <70693271+arnort20@users.noreply.github.com>
2021-05-13 17:14:29 +0000

    order takki

--- user/forms/order_form.py
+++ user/forms/order_form.py
@@ -5,10 +5,5 @@ from django.forms import ModelForm, widgets
 class OrderForm(ModelForm):
     class Meta:
         model = Orders
-        exclude = ['orderId', 'userId', 'date']
-        widgets = {
-            'cardId': widgets.Select(attrs={'class': 'form-control'
-                                            }),
-            'addrId': widgets.Select(attrs={'class': 'form-control'
-                                            })
-        }
+        exclude = ['orderId', 'date']
+
