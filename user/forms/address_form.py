74c5b59ccf195810e5c0e94743bc1f1d3b2dd252
Rut Tryggvadóttir <rutt20@ru.is>
2021-05-13 11:14:49 +0000

    haha

--- user/forms/address_form.py
+++ user/forms/address_form.py
@@ -10,12 +10,8 @@ class AddressCreateForm(ModelForm):
         widgets = {
             'street': widgets.TextInput(attrs={'class': 'form-control address__input'}),
             'apt_num': widgets.NumberInput(attrs={'class': 'form-control address__input-small'}),
-            'zip': widgets.NumberInput(attrs={'class': 'form-control address__input-small'}),
-<<<<<<< HEAD
-            #'country': widgets.Select(attrs={'class': 'form-control address__input'})
-=======
-            'country': CountrySelectWidget()
->>>>>>> 3fdc08a06f95d0204fb4205f8ea45ed7176dc539
+            'zip': widgets.NumberInput(attrs={'class': 'form-control address__input-small'})
         }
+
 
 
